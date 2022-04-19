7bbc9595c3870c4ca64e4c1ad2984bfa6849edaf
Emily  Phee <emilyphee@Emilys-MacBook-Air.local>
2022-04-19 11:43:26 -0700

    Finish Section 8: Array Chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunked = [];
+
+  array.forEach(c => {
+    const lastChunk = chunked[chunked.length - 1];
+    if (lastChunk === undefined || lastChunk.length === size) {
+      chunked.push([]);
+    }
+    chunked[chunked.length - 1].push(c);
+  });
+  return chunked;
+}
 
 module.exports = chunk;
