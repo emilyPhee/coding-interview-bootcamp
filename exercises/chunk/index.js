91f43147ee67acc9cea9a2f0d0819ef70cf98706
Emily  Phee <emilyphee@Emilys-MacBook-Air.local>
2022-04-29 12:43:51 -0700

    Finish Section 9: Anagrams

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -18,6 +18,18 @@ function chunk(array, size) {
     }
     chunked[chunked.length - 1].push(c);
   });
+
+  // solution from Udemy
+  // for (let element of array) {
+  //   const last = chunked[chunked.length - 1];
+  // last will have reference to the chunked array and able to change/add value
+
+  //   if (!last || last.length === size) {
+  //     chunked.push([element]);
+  //   } else {
+  //     last.push(element);
+  //   }
+  // }
   return chunked;
 }
 
